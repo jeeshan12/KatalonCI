07ee6445bd629b19a06523f32e01c996851fa881
Mohd Jeeshan <mohdjeeshan007@gmail.com>
2019-11-28 20:54:37 +0530

    Added the new Test

---
+++ Test Suites/DemoSuiteLogin.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description>Demo Suite Login</description>
+   <name>DemoSuiteLogin</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>27794448-2c13-4cf4-b83e-6712bdd5323b</testSuiteGuid>
+   <testCaseLink>
+      <guid>2e0c07c7-1c6e-471f-9075-adb03fb25565</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/DemoTestLogin</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
